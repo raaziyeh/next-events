c9d7a2a3e7f4bc536d5c1e74c2b90fa45a826a95
raziyeh <rsoradimoghaddam@gmail.com>
2023-02-16 09:56:18 +0330

    Displaying filtered events

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,14 +1,46 @@
 import { useRouter } from "next/router"
+import { getFilteredEvents } from "../../dummy-data"
+import EventList from "../../components/events/event-list"
 
 function FilteredEventsPage() {
 	const router = useRouter()
+	const filterData = router.query.slug
 
-	console.log(router.query)
-    // fetch events filtered by the specific filters
+	if (!filterData) {
+		return <div className="centered-message">Loading ...</div>
+	}
+
+	const filterYear = filterData[0]
+	const filterMonth = filterData[1]
+
+	const numYear = +filterYear
+	const numMonth = +filterMonth
+
+	if (
+		isNaN(numYear) ||
+		isNaN(numMonth) ||
+		numYear > 2030 ||
+		numYear < 2021 ||
+		numMonth < 1 ||
+		numMonth > 12
+	) {
+		return <div className="centered-message">Please choose valid filters</div>
+	}
+
+	const filteredEvents = getFilteredEvents({
+		year: numYear,
+		month: numMonth,
+	})
+
+	if (!filteredEvents || filteredEvents.length === 0) {
+		return (
+			<p className="centered-message">No events found for the chosen filter!</p>
+		)
+	}
 
 	return (
 		<div>
-			<h1> Filtered Events Page</h1>
+			<EventList items={filteredEvents} />
 		</div>
 	)
 }
